52766148250cbfdbbf72bf748883361972c78d09
shobhitchauhancs <105166000+shobhitchauhancs@users.noreply.github.com>
2022-10-11 17:45:57 +0530

    about added

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
+let tabs=document.querySelectorAll('.tab');
+let links=document.querySelectorAll('.links');
+let elements=document.querySelectorAll('.init');
+animate();
 window.addEventListener('scroll',animate);
 
 function animate(){
-     let elements=document.querySelectorAll('.init');
-        console.log(elements);
-     let windowheight=window.innerHeight;
-     let topoffset=elements[0].getBoundingClientRect().top;
-     let checkpoint=150;
+        let scrollheight=window.scrollY;
+        console.log(scrollheight);
 
-     if(topoffset<windowheight-checkpoint){
-        elements[0].classList.add('active');
-     }
+        let windowheight=window.innerHeight;
+        let heightperc=0.44*windowheight;
+        let checkpoint=100;
+        for(let i=0;i<elements.length;i++){
+            let topoffset=elements[i].getBoundingClientRect().top;
+            
+            if(topoffset<windowheight-checkpoint){
+                elements[i].classList.add('active');
+            }
+            
+        }
+        for(let j=0;j<tabs.length;j++){
+            let taboffset=tabs[j].getBoundingClientRect().top;
+            let tabbottomoffset=tabs[j].getBoundingClientRect().bottom;
+            
+            if(taboffset<windowheight-heightperc && tabbottomoffset>windowheight-heightperc){
+                links[j].classList.add('linkactive');
+            }
+            else
+            if(links[j].classList.contains('linkactive')){
+                links[j].classList.remove('linkactive')
+            }
+            
+        }
 }
